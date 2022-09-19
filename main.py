379501b857213a23f3e918aeab0028a026e6305d
Somraj Mondal <111075192+somrajmondal@users.noreply.github.com>
2022-09-19 12:17:28 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyautogui as pg
+import time
+
+pg.FAILSAFE=False
+
+while True:
+  time.sleep(40)
+  for i in range(0,120)
+    pg.moveTo(0,i*5)
+    pg.moveRel(10,10,1)
+  for i in range(0,50)
+    pg.press('enter)
